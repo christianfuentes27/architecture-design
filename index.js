1b33f1ea60c41d953146b74a75e87a60ea57789a
Christian <cjfuentes27@ieszaidinvergeles.org>
2022-10-28 20:54:05 +0200

    Fix js

--- index.js
+++ index.js
@@ -5,5 +5,3 @@ const links = document.querySelectorAll('li a');
 menu.addEventListener('click', () => {
     navbar.classList.toggle('show');
 });
-
-links.forEach(link => link.addEventListener('click', () => console.log(link)));
