d1ce88e38f24106be7dc17bb3c1d7fb81cedfbbc
Christian <cjfuentes27@ieszaidinvergeles.org>
2022-10-30 15:56:16 +0100

    Services responsive

--- index.js
+++ index.js
@@ -1,7 +1,4 @@
 const menu = document.querySelector('.menu-bars');
 const navbar = document.getElementById('navbar');
-const links = document.querySelectorAll('li a');
 
-menu.addEventListener('click', () => {
-    navbar.classList.toggle('show');
-});
+menu.addEventListener('click', () => navbar.classList.toggle('show'));
